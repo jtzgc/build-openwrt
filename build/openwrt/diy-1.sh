8d201f6a3dc54488cbf4a0cb336c2772663c8e76
jtzgc <63477893+jtzgc@users.noreply.github.com>
2023-05-13 07:33:10 +0800

    Update diy-1.sh

--- build/openwrt/diy-1.sh
+++ build/openwrt/diy-1.sh
@@ -13,7 +13,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # lede源码增加passwall,也适用于增加其他插件的方法
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 sed -i '$a src-git luci_passwall https://github.com/xiaorouji/openwrt-passwall.git;luci' feeds.conf.default
 sed -i '$a src-git mosdns https://github.com/QiuSimons/openwrt-mos' feeds.conf.default
